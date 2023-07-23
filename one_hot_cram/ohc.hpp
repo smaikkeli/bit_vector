1643db18c9094277a61138977e2c947e65caa388
schmaigul <miksuhok@gmail.com>
2023-07-23 20:45:55 +0300

    insertion not working

--- one_hot_cram/ohc.hpp
+++ one_hot_cram/ohc.hpp
@@ -1,78 +1,113 @@
 #include <iostream>
-#include <vector>
+#include <limits>
 #include "./../bit_vector/bv.hpp"
 
+typedef bv::simple_bv<16, 16384, 64, true, true, true> rle_bv;
+
  //One hot cram one-hot encodes each of the characters in a given string
-template<class dtype = char, class bit_vector = bv::bv>
+template<typename dtype = char, class bit_vector = rle_bv>
 class onehotcram{
   private: 
-    size_t size_;  //The number of elements inserted
-    size_t alphabet_size;
-    uint64_t* alphabet; //alphabet, includes the number of bits used for each symbol
-    bit_vector* bv; //Pointer to the bitvector beneath the cumulative sums
+    size_t size_ {0};  //The size of the container
+    size_t alphabet_size_ {static_cast<size_t>(std::numeric_limits<dtype>::max()) + 1}; //The number of possible values for dtype
+    uint64_t* alphabet_bits {new uint64_t [alphabet_size_]}; //Starting index for each character, alphabet_bits[0] = 0
+    bit_vector* bv {new bit_vector}; //Pointer to the bitvector beneath the cumulative sums
 
   public:
-    onehotcram() : 
-      size_(0),  
-      alphabet_size((1UL << sizeof(dtype)) - 1), //Alphabet size is the maximum integer i.e. char = 255
-      alphabet(new uint64_t [(1UL << sizeof(dtype)) - 1]), 
-      bv(new bit_vector) {}
+    // @brief Default constructor, initializes the size to 0
+    onehotcram() {}
 
+    // @brief Constructor, allows defining an initial size
+    onehotcram(size_t input_size) : size_(input_size) {
+      uint64_t bv_size = input_size*alphabet_size_;
+        
+        //fill bitvector
+        for (uint64_t i = 0; i < bv_size; i++) {
+          bv->insert(0,0);
+        } 
+        
+        //write number of bits used for each character
+        for (uint64_t i = 0; i < alphabet_size_; i++) {
+          alphabet_bits[i] = i*input_size;
+        }
+      }
+
+    size_t bv_size() const {
+      return bv->size();
+    }
+    
     size_t size() const {
       return size_;
     }
-    
-    dtype at(uint64_t index) {
-      for (size_t i = 0; i < alphabet_size; i++) {
-        bool val = bv->at(alphabet[i] + index);
+
+    size_t alphabet_size() const {
+      return alphabet_size_;
+    }
+
+    dtype at(uint32_t index) {
+      for (size_t i = 0; i < alphabet_size_; i++) {
+        //std::cout << bv->at(alphabet_bits[i] + index) << std::endl;
+        bool val = bv->at(alphabet_bits[i] + index);
         if (val) {
-          std::cout << "found" << std::endl;
           return static_cast<dtype>(i);
         } 
       }
     }
 
-    void insert(uint64_t index, dtype elem) {
-      
-      if (index > size_) {
-        std::cout << "index " << index << " out of range" << std::endl;
-        return;
-      }
-      //Initialize new 0's for the insertion, then set!!!!!! 
-      if (size_ == 0) {
-        std::cout << "started initialization " << std::endl;
-        for (size_t i = 0; i < alphabet_size; i++) {
-          bv->insert(0, 0);
-          std::cout << "insert" << std::endl;
-          if (uint64_t(i) == uint64_t(elem)) {
-            bv->set((alphabet[i] + index), 1);
-            std::cout << "set" << std::endl;
-            size_++;
-          }
-        }
-        return;
-      }
+    void insert(uint32_t index, dtype elem) {
 
-      bv->insert(index, (0UL == uint64_t(elem)));
-      //Naive implementation, add one bit for each element in the alphabet
-      for (size_t i = 1; i < alphabet_size; i++) {
-        alphabet[i] = alphabet[i-1] + 1; 
-        bv->insert((alphabet[i] + index), (uint64_t(i) == uint64_t(elem)));
-        std::cout << "inserted at " << i << std::endl;
-      }
+      if (size_ == 0) {
+        std::cout << "size is 0 " << std::endl;
+        for (size_t i = 0; i < alphabet_size_; i++) {
+          bv->insert(i, (i == size_t(elem)));
+          if (i == size_t(elem)) {
+            std::cout << " inserted element " << elem << " at bitvector index " << i << std::endl; 
+          }
+          alphabet_bits[i] = i;
+          std::cout << "Starting bit for element " << static_cast<dtype>(i) << " is " << alphabet_bits[i] << std::endl;
+        }
+      } else { 
+         //Loop through each element in the alphabet, add 0 or 1 
+         for (size_t i = 0; i < alphabet_size_ - 1; i++) {
+          if (i == size_t(elem)) {
+           std::cout << i << " == " << size_t(elem) << std::endl;
+           std::cout << alphabet_bits[i] + index  << " is the bitvector index" << std::endl;
+          }
+          bv->insert((alphabet_bits[i] + index), (i == size_t(elem)));
+          alphabet_bits[i + 1]++;
+          //Increment the next index, as insert(i) moves each j >= i by 1 
+        }
+      } 
+      bv->insert(alphabet_bits[alphabet_size_-1], ((alphabet_size_ - 1) == size_t(elem)));
       size_++;
     }
 
     bool remove(uint64_t index) {
+      for (size_t i = 0; i < alphabet_size_; i++) {
+        uint32_t bv_index = alphabet_bits[i] + index;
+        if (bv->at(bv_index)) {
+             return bv->remove(bv_index);
+            }
+      }
       return false;
     }
 
-    bool set(uint64_t index, dtype elem) {
-      return false;
+    void set(uint64_t index, dtype elem) {
+      for (size_t i = 0; i < alphabet_size_; i++) {
+        uint32_t bv_index = alphabet_bits[i] + index;
+        //Unset the old value
+        if (bv->at(bv_index)) {
+          bv->set((bv_index), false);
+        }
+        //Set the new value
+        if (i == size_t(elem)) {
+          bv->set((bv_index), true);
+        }
+      }
     }
-
-    uint64_t rank(uint64_t index) {
-      return 0;
+    
+    uint64_t rank(dtype elem) {
+      return bv->rank(alphabet_bits[elem] + size_) - bv->rank(alphabet_bits[elem]);
     }
 
     uint64_t select(uint64_t index) {
--- onehotcram_test.cpp
+++ onehotcram_test.cpp
@@ -2,6 +2,7 @@
 #include <sys/resource.h>
 
 #include <cassert>
+#include <string>
 #include <chrono>
 #include <iomanip>
 #include <iostream>
@@ -10,11 +11,21 @@
 #include "one_hot_cram/ohc.hpp"
 #include "bit_vector/bv.hpp"
 
+typedef bv::simple_bv<16, 16384, 64, true, true, true> rle_bv;
+
 int main (int argc, char *argv[]) {
-  onehotcram onehot;
-  char a = 65;
-  onehot.insert(0, a);
-  std::cout << onehot.at(0) << std::endl;
+  onehotcram<char> onehot;
+  std::string test = "banana";
+
+  for(size_t i = 0; i<test.size(); i++) {
+        onehot.insert(i, test[i]);
+    }
+  std::cout << "alphabet size: " << onehot.alphabet_size() << std::endl;
+  std::cout << "size of the datastructure: " << onehot.size() << std::endl;
+  std::cout << "bitvector size: " << onehot.bv_size() << std::endl;
+  for (size_t i = 0; i<test.size(); i++ ) {
+    std::cout << onehot.at(i) << std::endl;
+  }
 
   return 0;
 };
